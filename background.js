a36663695352712e3a4009559f2be4c091be866a
Louis Chavey <louis@chavey.org>
2025-12-03 11:42:28 -0600

    different popup for instagram

--- background.js
+++ background.js
@@ -1,19 +1,32 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    const instagramConditions = [
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'www.instagram.com' }
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'instagram.com' }
-      })
-    ];
-
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: instagramConditions,
-        actions: [ new chrome.declarativeContent.ShowAction() ]
-      }
-    ]);
+function setPopupForTab(tabId, url) {
+  if (!url) return;
+  const isInstagram = /(^|\.)instagram\.com\//.test(url);
+  chrome.action.setPopup({
+    tabId,
+    popup: isInstagram ? 'popup/popup_instagram.html' : 'popup/popup.html'
   });
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url) {
+    setPopupForTab(tabId, tab.url);
+  }
+});
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  try {
+    const tab = await chrome.tabs.get(activeInfo.tabId);
+    setPopupForTab(activeInfo.tabId, tab.url);
+  } catch (e) {
+    // ignore
+  }
+});
+
+chrome.runtime.onInstalled.addListener(async () => {
+  try {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab) setPopupForTab(tab.id, tab.url);
+  } catch (e) {
+    // ignore
+  }
 });
